009e06f800cdafae745976fcdc2aebef3495a8cc
Adraina Cheeks <programmingwithacheeks@xpanxion.com>
2022-08-18 10:26:34 -0400

    adding data to repo

---
+++ src/BillingReport.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import './BillingReport.css'
+
+export const BillingReport = () => {
+  return (
+    <div>BillingReport</div>
+  )
+}
---
+++ src/TopPanel.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { Outlet, Link } from "react-router-dom";
+import './TopPanel.css'
+
+const Layout = () => {
+  return (
+    <>
+    <div className="links">
+            <Link to={`/`}><button className="customer-button">Customer</button></Link>&nbsp;&nbsp;
+            <Link to={`/technician`}><button className="tech-button">Technician</button></Link>
+            <Link to={`/billing`}><button className="billing-button">Billing Report</button></Link>
+      <Outlet />
+      </div>
+    </>
+  )
+};
+
+export default Layout;
