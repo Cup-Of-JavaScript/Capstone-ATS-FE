3d574689b46a6ac5fed03c6f0721668681476e76
Adraina Cheeks <105386345+KinkyHairCoder@users.noreply.github.com>
2022-08-18 15:32:20 -0400

    Revert "Get customer and technician"

--- src/Customer.js
+++ src/Customer.js
@@ -1,92 +1,79 @@
 import React from "react";
-import axios from "axios";
 import "./Customer.css";
-import { useState, useEffect } from "react";
+import { useState } from "react";
 
 const Customer = () => {
-    const [selectedOptions, setSelectedOptions] = useState('');
-    const [urgentOptions, setUrgentOptions] = useState('');
+  const [selectedOptions, setSelectedOptions] = useState('');
+  const [urgentOptions, setUrgentOptions] = useState('');
 
-    useEffect(() => {
-        const fetch = async () => {
-            let r = await axios.get(`http://localhost:5150/category`)
-            setSelectedOptions(r.data)
-        }
-        fetch()
-    }, [])
+  const categoryList = [
+    { value: "paint", label: "Paint" },
+    { value: "plumbing", label: "Plumbing" },
+    { value: "electrical", label: "Electrical" },
+  ];
 
-    // const categoryList = [
-    //     { value: "paint", label: "Paint" },
-    //     { value: "plumbing", label: "Plumbing" },
-    //     { value: "electrical", label: "Electrical" },
-    // ];
+  const list = [
+    { value: "high", label: "High" },
+    { value: "normal", label: "Normal" },
+  ];
 
-    // const list = [
-    //     { value: "high", label: "High" },
-    //     { value: "normal", label: "Normal" },
-    // ];
+  const onhandleSelect = (e) => {
+    setSelectedOptions(e);
+  };
 
-    const onhandleSelect = async (selectedOptions) => {
-        setSelectedOptions(selectedOptions);
-        let result = await axios.get(`http://localhost:5150/category`)
-        selectedOptions(result.data)
-    };
-
-    const onhandleClick = async (urgentOptions) => {
-        setUrgentOptions(urgentOptions);
-        let result = await axios.get(`http://localhost:5150/Urgency`)
-        urgentOptions(result.data)
-    };
+  const onhandleClick = (e) => {
+    setUrgentOptions(e);
+  };
 
 
-    return (
-        <div className="customer">
-            <div className="customer-name">
-                Customer Name:
-            </div>
-            <input
-                className="input-name"
-                onClick={() => { }}
-                placeholder="Full Name"
-            ></input>
-            <div className="category">
-                Category:
-            </div>
-            <select
-                value={selectedOptions}
-                onChange={(e) => onhandleSelect(e.target.value)}
-            >
-                <option value="">
-                    Select Category
-                </option>
-                {selectedOptions.map((s) => (
-                    <option key={s.value} value={s.value}>
-                        {s.label}{" "}
-                    </option>
-                ))}
-            </select>
-            <div className="urgency-select">
-                <div className="urgency">
-                    Urgency:
-                </div>
-                <select
-                    value={urgentOptions}
-                    onChange={(e) => onhandleClick(e.target.value)}>
-                    <option value="">
-                        Select Category
-                    </option>
-                    {urgentOptions.map((s) => (
-                        <option key={s.value} value={s.value}>
-                            {s.label}{" "}
-                        </option>
-                    ))}
-                </select>
-            </div>
-            <div className="submit-button">
-                <button className="btn">Submit</button>
-            </div>
-        </div >
-    );
+  return (
+    <div className="customer">
+      <div className="customer-name">
+        Customer Name:
+      </div>
+      <input
+        className="input-name"
+        onClick={() => { }}
+        placeholder="Full Name"
+      ></input>
+      <div className="category">
+        Category:
+      </div>
+      <select
+        value={selectedOptions}
+        onChange={(e) => onhandleSelect(e.target.value)}
+      >
+        <option value="">
+          Select Category
+        </option>
+        {categoryList.map((s) => (
+          <option key={s.value} value={s.value}>
+            {s.label}{" "}
+          </option>
+        ))}
+      </select>
+      <div className="urgency-select">
+        <div className="urgency">
+          Urgency:
+        </div>
+        <select
+          value={urgentOptions}
+          onChange={(e) => onhandleClick(e.target.value)}>
+          <option value="">
+            Select Category
+          </option>
+          {list.map((s) => (
+            <option key={s.value} value={s.value}>
+              {s.label}{" "}
+            </option>
+          ))}
+        </select>
+      </div>
+      <div className="submit-button">
+        <button className="btn">Submit</button>
+      </div>
+    </div >
+  );
 };
 
 export default Customer;
