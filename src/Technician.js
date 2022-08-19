e4bf3024923df7630436415cc1a2bf41eaab3048
Adraina Cheeks <programmingwithacheeks@xpanxion.com>
2022-08-19 14:15:00 -0400

    drop downs down

--- src/Technician.js
+++ src/Technician.js
@@ -1,53 +1,75 @@
-import React from "react";
-import { useState } from "react";
+import { React, useState, useEffect } from "react";
+import axios from 'axios'
 import './Technician.css'
 
 const Technician = () => {
-    const [selectedOptions, setSelectedOptions] = useState("");
-    const [otherSelectedOptions, setOtherSelectedOptions] = useState("")
+    const [selectedOptions, setSelectedOptions] = useState([]);
+    const [otherSelectedOptions, setOtherSelectedOptions] = useState([])
+    const [techListId, setTechListId] = useState(0)
+    const [issueListId, setIssueListId] = useState(0)
 
-    const people = [
-        { value: "Alice", label: "Alice" },
-        { value: "Bob", label: "Bob" },
-        { value: "Charlie", label: "Charlie" },
-    ];
+    useEffect(() => {
+        const fetch = async () => {
+            let r = await axios.get(`http://localhost:5150/Technician`)
+            setSelectedOptions(r.data)
+        }
+        fetch()
+    }, [])
 
-    const problems = [
-        { value: "Paint", label: "Paint" },
-        { value: "Plumbing", label: "Plumbing" },
-        { value: "Electrical", label: "Electrical" },
-    ];
+    useEffect(() => {
+        const fetch = async () => {
+            let r = await axios.get(`http://localhost:5150/category`)
+            setOtherSelectedOptions(r.data)
+        }
+        fetch()
+    }, [])
 
-    const onHandleSelect = (e) => {
-        setSelectedOptions(e);
+    // const people = [
+    //     { value: "Alice", label: "Alice" },
+    //     { value: "Bob", label: "Bob" },
+    //     { value: "Charlie", label: "Charlie" },
+    // ];
+
+    // const problems = [
+    //     { value: "Paint", label: "Paint" },
+    //     { value: "Plumbing", label: "Plumbing" },
+    //     { value: "Electrical", label: "Electrical" },
+    // ];
+
+    const onHandleSelect = async (technician) => {
+        setSelectedOptions(technician);
+        console.log(techListId)
+        let result = await axios.get(`http://localhost:5150/Technician`)
+        setTechListId(result.data)
     };
 
-    const onOtherHandleSelect = (e) => {
-        setOtherSelectedOptions(e);
+    const onOtherHandleSelect = async (category) => {
+        setOtherSelectedOptions(category);
+        console.log(issueListId)
+        let result = await axios.get(`http://localhost:5150/category`)
+        setIssueListId(result.data)
     };
 
     return (
         <div className="Technician">
             <div>Technician Name:</div>
             <select
-                value={selectedOptions}
                 onChange={(e) => onHandleSelect(e.target.value)}>
                 <option value="">Select Name</option>
-                {people.map((s) => (
-                    <option key={s.value} value={s.value}>
-                        {s.label}{" "}
+                {selectedOptions.map((s) => (
+                    <option key={s.tech_id} value={s.tech_id}>
+                        {s.tech_name}
                     </option>
                 ))}
             </select>
             <div>
                 <div>Issue</div>
                 <select
-                    value={otherSelectedOptions}
                     onChange={(e) => onOtherHandleSelect(e.target.value)}>
                     <option value="">Select Issue</option>
-                    {problems.map((s) => (
-                        <option key={s.value} value={s.value}>
-                            {s.label}{" "}
+                    {otherSelectedOptions.map((s) => (
+                        <option key={s.category_id} value={s.category_id}>
+                            {s.category_name}
                         </option>
                     ))}
                 </select>
