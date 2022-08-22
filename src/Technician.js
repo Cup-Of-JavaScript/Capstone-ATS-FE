d99ee4c0d4da67872ec0b01ffb0f1a9fc58472aa
Adraina Cheeks <programmingwithacheeks@xpanxion.com>
2022-08-22 11:59:04 -0400

    fixed console errors

--- src/Technician.js
+++ src/Technician.js
@@ -1,12 +1,10 @@
-import { React, useState, useEffect } from "react";
+import { React, useState, useEffect} from "react";
 import axios from 'axios'
 import './Technician.css'
 
 const Technician = () => {
     const [selectedOptions, setSelectedOptions] = useState([]);
     const [otherSelectedOptions, setOtherSelectedOptions] = useState([])
-    const [techListId, setTechListId] = useState(0)
-    const [issueListId, setIssueListId] = useState(0)
 
     useEffect(() => {
         const fetch = async () => {
@@ -24,25 +22,10 @@ const Technician = () => {
         fetch()
     }, [])
 
-    const onHandleSelect = async (technician) => {
-        setSelectedOptions(technician);
-        console.log(techListId)
-        let result = await axios.get(`http://localhost:5150/Technician`)
-        setTechListId(result.data)
-    };
-
-    const onOtherHandleSelect = async (category) => {
-        setOtherSelectedOptions(category);
-        console.log(issueListId)
-        let result = await axios.get(`http://localhost:5150/category`)
-        setIssueListId(result.data)
-    };
-
     return (
         <div className="Technician">
             <div>Technician Name:</div>
-            <select
-                onChange={(e) => onHandleSelect(e.target.value)}>
+            <select>
                 <option value="">Select Name</option>
                 {selectedOptions.map((s) => (
                     <option key={s.tech_id} value={s.tech_id}>
@@ -52,8 +35,7 @@ const Technician = () => {
             </select>
             <div>
                 <div>Issue</div>
-                <select
-                    onChange={(e) => onOtherHandleSelect(e.target.value)}>
+                <select>
                     <option value="">Select Issue</option>
                     {otherSelectedOptions.map((s) => (
                         <option key={s.category_id} value={s.category_id}>
