79760fc7895ab4b10403226c95009517e4aef9ec
Antionette Van <avan@xpanxion.com>
2022-08-23 10:07:53 -0400

    Update

--- src/Technician.js
+++ src/Technician.js
@@ -2,6 +2,7 @@ import { React, useState, useEffect, useRef } from "react";
 import axios from 'axios'
 import './Technician.css'
 
+
 const Technician = () => {
     const [selectedOptions, setSelectedOptions] = useState([]);
     const [otherSelectedOptions, setOtherSelectedOptions] = useState([])
